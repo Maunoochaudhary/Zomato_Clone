cbe09b434af51e1ff8e876fb67b62b88a5cd0043
monu <mcsgitjaipur@gmail.com>
2023-02-01 10:31:31 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import {View, StyleSheet,Text} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({})
+
+export default App;
